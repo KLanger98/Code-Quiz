a823ae97a104870db245e286888093ed7bfff15c
KL <karl.w.langer@gmail.com>
2024-01-03 14:21:54 +1030

    Added timer function to scriptwhich activitates on startGame button click

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,33 @@
 
-
 //begin game function
 //Start timer using a setInterval function 
 //load first question
+function startGame(){
+    setTimer();
+    //start Timer
+    //load first question
+}
+
+function loadQuestion(){
+
+}
+
+var timerBox = document.getElementById('timer');
+
+function setTimer(){
+    var timeLeft = 2;
+
+    var timer = setInterval(function(){
+    timeLeft--;
+    timerBox.innerText = timeLeft
+    if(timeLeft == 0){
+        clearInterval(timer)
+        timerBox.innerText = ""
+    }
+},
+1000)
+}
 
 
-
-//Check high scores function
+//Check high scores function 
+    //
