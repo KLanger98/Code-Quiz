8482c623512a2c7866b0276cf905d30d1a78fe60
KL <karl.w.langer@gmail.com>
2024-01-03 15:13:12 +1030

    Added a function that will cycle through questions and add correct answers to a score once an option is selected

--- Assets/script.js
+++ Assets/script.js
@@ -3,54 +3,32 @@
 var stage = document.getElementById('stage');
 var timerBox = document.getElementById('timer');
 
-let questionNumber = 0;
+var questionNumber = 0;
+let score = 0;
 
 let quizQuestions = [
     {
         question: "Hello",
-        optionA: "A",
-        optionB: "B",
-        optionC: "C",
-        optionD: "D",
-        correctAnswer: "A"
+        options: ["a", "b", "c", "d"],
+        correctAnswer: "a"
     },
     {
         question: "Hello",
-        optionA: "A",
-        optionB: "B",
-        optionC: "C",
-        optionD: "D",
-        correctAnswer: "A"
+        options: ["d", "e", "f", "g"],
+        correctAnswer: "e"
     }
     
 ]
-//begin game function
-//Start timer using a setInterval function 
-//load first question
+
 function startGame(){
     setTimer();
     clearStage();
+
+    score = 0;
     
     questionNumber = 0;
     loadQuestion(questionNumber);
 }
-
-function loadQuestion(questionNumber){
-    let currentQuestion = document.createElement('h2');
-    currentQuestion.innerText = quizQuestions[questionNumber].question;
-    let answer = quizQuestions[questionNumber].correctAnswer;
-
-    let optionA = quizQuestions[questionNumber].optionA;
-    let optionB = quizQuestions[questionNumber].optionB;
-    let optionC = quizQuestions[questionNumber].optionC;
-    let optionD = quizQuestions[questionNumber].optionD;
-
-    stage.append(currentQuestion);
-    
-    questionNumber++;
-}
-
-
 
 function setTimer(){
     var timeLeft = 60;
@@ -71,6 +49,52 @@ function clearStage(){
         stage.removeChild(stage.firstChild);
     }
 }
+
+function loadQuestion(){
+    let currentQuestion = document.createElement('h2');
+
+    console.log(questionNumber)
+    currentQuestion.innerText = quizQuestions[questionNumber].question;
+    let answer = quizQuestions[questionNumber].correctAnswer;
+
+    let optionsList = document.createElement('ul');
+    
+    for(let i = 0; i < 4; i++){
+        let option = document.createElement('li');
+        option.innerText = quizQuestions[questionNumber].options[i];
+        option.setAttribute("id", "option" + i)
+        option.setAttribute('class', "quizOption")
+        option.addEventListener('click', () => {
+            if(answer === option.innerText){
+                console.log('Correct');
+                nextQuestion(true);
+            } else{
+                console.log('Wrong');
+                nextQuestion(false);
+            }
+        })
+
+        optionsList.append(option);
+    }
+
+    stage.append(currentQuestion);
+    stage.append(optionsList)
+}
+
+function nextQuestion(answer){
+    if(answer == true){
+        score++;
+    } 
+
+    clearStage();
+    questionNumber++;
+    loadQuestion();
+}
+
+
+
+
+
 
 //Check high scores function 
     //
